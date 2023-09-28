9c94e24f07e59f9af6bdf0eaadb0a5edc7d90c2f
Slokh <slokhx@gmail.com>
2023-09-27 22:59:21 -0400

    fix: cast embeds for urls

--- components/actions/new-cast.tsx
+++ components/actions/new-cast.tsx
@@ -47,104 +47,15 @@ const formSchema = z.object({
   embeds: z.array(z.string().url()).max(2),
 });
 
-export const NewCast = ({
-  parent,
-  header,
-  children,
+const NewCastFileUpload = ({
+  onFileUpload,
+  isDisabled,
 }: {
-  parent?: FarcasterCast;
-  header?: React.ReactNode;
-  children: React.ReactNode;
+  onFileUpload: (embed: Embed) => void;
+  isDisabled: boolean;
 }) => {
-  const { authState } = useUser();
-
-  const [loadingChannel, setLoadingChannel] = useState(true);
-  const [loading, setLoading] = useState(false);
-  const { signerUuid, user } = useUser();
-  const [embeds, setEmbeds] = useState<string[]>([]);
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      text: "",
-      embeds: [],
-    },
-  });
-  const [url, setUrl] = useState<string | undefined>(undefined);
-  const [urlEmbed, setUrlEmbed] = useState<Embed | undefined>(undefined);
-
-  const pathname = usePathname();
-  const [channel, setChannel] = useState<Channel | undefined>(undefined);
-
-  useEffect(() => {
-    if (!parent && pathname.includes("/channels/")) {
-      setChannel(CHANNELS_BY_ID[pathname.split("/")[2]]);
-    }
-    setLoadingChannel(false);
-  }, [parent, pathname]);
-
-  const {
-    watch,
-    formState: { errors },
-  } = form;
-  const textValue = watch("text");
-
-  useEffect(() => {
-    const fetchEmbed = async () => {
-      const res = await fetch("/api/embeds", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ url }),
-      });
-      const metadata = await res.json();
-      setUrlEmbed(metadata);
-    };
-    if (url) {
-      fetchEmbed();
-    } else {
-      setUrlEmbed(undefined);
-    }
-  }, [url]);
-
-  useEffect(() => {
-    form.trigger("text");
-
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
-    const urls = textValue.match(urlRegex);
-    if (urls) {
-      setUrl(urls[0]);
-    } else {
-      setUrl(undefined);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [textValue]);
-
-  const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    setLoading(true);
-    const res = await fetch("/api/casts", {
-      method: "POST",
-      body: JSON.stringify({
-        signer_uuid: signerUuid,
-        text: values.text,
-        embeds: embeds.map((url) => ({ url })),
-        parent: parent?.hash || channel?.parentUrl,
-      }),
-    });
-    const { hash } = await res.json();
-    while (true) {
-      const res2 = await fetch(`/api/casts/${hash}`);
-      if (res2.ok) {
-        // @ts-ignore
-        window.location = `/${user?.fname}/${hash}`;
-        break;
-      }
-      await new Promise((resolve) => setTimeout(resolve, 200));
-    }
-  };
-
   const handleFileSelect = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && embeds.length < 2) {
+    if (e.target.files && !isDisabled) {
       const file = e.target.files[0];
       // Read the file as a data URL (base64)
       const reader = new FileReader();
@@ -166,54 +77,183 @@ export const NewCast = ({
           body: JSON.stringify({ image: base64string }),
         });
 
+        if (res.status !== 200) {
+          return;
+        }
+
         const { data } = await res.json();
-        setEmbeds([...embeds, data.link]);
+        onFileUpload({
+          url: data.link,
+          urlHost: "imgur.com",
+          contentType: "image/png",
+          parsed: false,
+        });
       };
     }
   };
 
-  if (authState !== UserAuthState.LOGGED_IN) return <></>;
+  return (
+    <>
+      <label htmlFor="embed">
+        {isDisabled ? (
+          <div className="p-2 cursor-pointer border rounded-lg text-zinc-500">
+            <CameraIcon className="h-4 w-4" />
+          </div>
+        ) : (
+          <div className="p-2 cursor-pointer border rounded-lg">
+            <CameraIcon className="h-4 w-4" />
+          </div>
+        )}
+      </label>
+      <Input
+        id="embed"
+        type="file"
+        className="hidden"
+        onChange={handleFileSelect}
+        disabled={isDisabled}
+        accept="image/*"
+      />
+    </>
+  );
+};
+
+const NewCastContent = ({ parent }: { parent?: string }) => {
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
+  const { signerUuid, user } = useUser();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      text: "",
+      embeds: [],
+    },
+  });
+
+  const [embeds, setEmbeds] = useState<Embed[]>([]);
+  const [files, setFiles] = useState<Embed[]>([]);
+  const [fetchedEmbeds, setFetchedEmbeds] = useState<Record<string, Embed>>({});
+
+  const pathname = usePathname();
+  const [channel, setChannel] = useState<Channel | undefined>(undefined);
+
+  useEffect(() => {
+    if (!parent && pathname.includes("/channels/")) {
+      setChannel(CHANNELS_BY_ID[pathname.split("/")[2]]);
+    }
+    setLoading(false);
+  }, [parent, pathname]);
+
+  const {
+    watch,
+    formState: { errors },
+  } = form;
+  const textValue = watch("text");
+
+  useEffect(() => {
+    form.trigger("text");
+
+    const fetchEmbed = async (url: string) => {
+      if (fetchedEmbeds[url]) return fetchedEmbeds[url];
+      const res = await fetch("/api/embeds", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url }),
+      });
+      const metadata = await res.json();
+      return metadata;
+    };
+
+    const fetchEmbeds = async (urls: string[]) => {
+      const newFetchedEmbeds = (await Promise.all(urls.map(fetchEmbed)))
+        .filter(
+          ({ contentMetadata }) => Object.keys(contentMetadata).length > 0
+        )
+        .slice(0, 2 - files.length);
+      const newFetchedEmbedsMap = newFetchedEmbeds.reduce(
+        (acc, embed) => ({ ...acc, [embed.url]: embed }),
+        {}
+      );
+      setFetchedEmbeds({ ...fetchedEmbeds, ...newFetchedEmbedsMap });
+      setEmbeds(newFetchedEmbeds);
+    };
+
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    const urls = textValue.match(urlRegex);
+    if (urls && files.length < 2) {
+      fetchEmbeds(urls);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [textValue]);
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    setSubmitting(true);
+    const urls = [
+      ...files.map((file) => ({
+        url: file.url,
+      })),
+      ...embeds.map((embed) => ({
+        url: embed.url,
+      })),
+    ];
+    const res = await fetch("/api/casts", {
+      method: "POST",
+      body: JSON.stringify({
+        signer_uuid: signerUuid,
+        text: values.text,
+        embeds: urls,
+        parent: parent || channel?.parentUrl,
+      }),
+    });
+    const { hash } = await res.json();
+    while (true) {
+      const res2 = await fetch(`/api/casts/${hash}`);
+      if (res2.ok) {
+        // @ts-ignore
+        window.location = `/${user?.fname}/${hash}`;
+        break;
+      }
+      await new Promise((resolve) => setTimeout(resolve, 200));
+    }
+  };
 
   return (
-    <Dialog>
-      <DialogTrigger>{children}</DialogTrigger>
-      <DialogContent>
-        {header}
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="text"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Textarea
-                      className="resize-none"
-                      placeholder="Type your message here..."
-                      {...field}
-                      onChange={(e) => {
-                        field.onChange(e); // preserve the original onChange event from react-hook-form
-                        e.target.style.height = "inherit";
-                        e.target.style.height = `${e.target.scrollHeight}px`;
-                      }}
-                    />
-                  </FormControl>
-                  {errors.text && (
-                    <FormMessage>{errors.text.message}</FormMessage>
-                  )}
-                </FormItem>
-              )}
-            />
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="text"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Textarea
+                  className="resize-none"
+                  placeholder="Type your message here..."
+                  {...field}
+                  onChange={(e) => {
+                    field.onChange(e); // preserve the original onChange event from react-hook-form
+                    e.target.style.height = "inherit";
+                    e.target.style.height = `${e.target.scrollHeight}px`;
+                  }}
+                />
+              </FormControl>
+              {errors.text && <FormMessage>{errors.text.message}</FormMessage>}
+            </FormItem>
+          )}
+        />
+        <ScrollArea>
+          <div className="flex flex-col">
             <div className="flex flex-row">
-              {embeds.map((embed, i) => (
-                <div key={embed} className="m-2 relative">
-                  <img src={embed} alt="embed" />
+              {files.map((file, i) => (
+                <div key={file.url} className="m-2 relative">
+                  <img src={file.url} alt="embed" />
                   <div
                     className=" absolute top-0 right-0 cursor-pointer bg-background rounded-full p-1 m-1"
                     onClick={() => {
-                      const newEmbeds = [...embeds];
-                      newEmbeds.splice(i, 1);
-                      setEmbeds(newEmbeds);
+                      const newFiles = [...files];
+                      newFiles.splice(i, 1);
+                      setFiles(newFiles);
                     }}
                   >
                     <Cross1Icon className="h-2 w-2" />
@@ -221,47 +261,71 @@ export const NewCast = ({
                 </div>
               ))}
             </div>
-            {urlEmbed && (
-              <div className="mt-2">
-                <EmbedPreview embed={urlEmbed} />
-              </div>
-            )}
-            <div className="flex flex-row justify-between items-center mt-2 space-x-2">
-              <label htmlFor="embed">
-                <div className="p-2 cursor-pointer border rounded-lg">
-                  <CameraIcon className="h-4 w-4" />
+            {embeds.map((embed, i) => (
+              <div key={embed.url} className="m-2 relative">
+                <EmbedPreview embed={embed} />
+                <div
+                  className=" absolute top-0 right-0 cursor-pointer bg-background rounded-full p-1 m-1"
+                  onClick={() => {
+                    const newEmbeds = [...embeds];
+                    newEmbeds.splice(i, 1);
+                    setEmbeds(newEmbeds);
+                  }}
+                >
+                  <Cross1Icon className="h-2 w-2" />
                 </div>
-              </label>
-              <Input
-                id="embed"
-                type="file"
-                className="hidden"
-                onChange={handleFileSelect}
-              />
-              <div className="flex flex-row space-x-2 items-center">
-                {!loadingChannel && !parent && (
-                  <ChannelSelect
-                    channel={channel?.channelId}
-                    onChange={(value: string) =>
-                      setChannel(CHANNELS_BY_ID[value])
-                    }
-                    disableAll
-                  />
-                )}
-                <Button type="submit" disabled={loading}>
-                  {loading ? <Loading /> : "Cast"}
-                </Button>
               </div>
-            </div>
-          </form>
-        </Form>
+            ))}
+          </div>
+        </ScrollArea>
+        <div className="flex flex-row justify-between items-center mt-2 space-x-2">
+          <NewCastFileUpload
+            isDisabled={files.length + embeds.length >= 2}
+            onFileUpload={(file) => setFiles([...files, file])}
+          />
+          <div className="flex flex-row space-x-2 items-center">
+            {!loading && !parent && (
+              <ChannelSelect
+                channel={channel?.channelId}
+                onChange={(value: string) => setChannel(CHANNELS_BY_ID[value])}
+                disableAll
+              />
+            )}
+            <Button type="submit" disabled={submitting}>
+              {submitting ? <Loading /> : "Cast"}
+            </Button>
+          </div>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+const NewCastDialog = ({
+  parent,
+  header,
+  children,
+}: {
+  parent?: FarcasterCast;
+  header?: React.ReactNode;
+  children: React.ReactNode;
+}) => {
+  const { authState } = useUser();
+  if (authState !== UserAuthState.LOGGED_IN) return <></>;
+
+  return (
+    <Dialog>
+      <DialogTrigger>{children}</DialogTrigger>
+      <DialogContent>
+        {header}
+        <NewCastContent parent={parent?.hash} />
       </DialogContent>
     </Dialog>
   );
 };
 
 export const NewCastButton = () => (
-  <NewCast
+  <NewCastDialog
     header={
       <DialogHeader>
         <DialogTitle>New cast</DialogTitle>
@@ -273,11 +337,11 @@ export const NewCastButton = () => (
       <PlusIcon />
       New cast
     </div>
-  </NewCast>
+  </NewCastDialog>
 );
 
 export const ReplyCastButton = ({ parent }: { parent: FarcasterCast }) => (
-  <NewCast
+  <NewCastDialog
     header={
       <ScrollArea className="max-h-96">
         <CastContent cast={parent} />
@@ -286,5 +350,5 @@ export const ReplyCastButton = ({ parent }: { parent: FarcasterCast }) => (
     parent={parent}
   >
     <div className="hover:underline">reply</div>
-  </NewCast>
+  </NewCastDialog>
 );
