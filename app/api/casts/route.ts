3979d44e0b92b9a8f92240cc6234da5d80d91e04
Slokh <slokhx@gmail.com>
2024-02-05 11:19:50 -0500

    try: cache

--- app/api/casts/route.ts
+++ app/api/casts/route.ts
@@ -1,70 +1,111 @@
 import {
-  getCastsResponseByHotness,
-  getCastsResponseByNewness,
-  getCastsResponseByTopLikes,
+	getCastsResponseByHotness,
+	getCastsResponseByNewness,
+	getCastsResponseByTopLikes,
 } from "@/lib/casts";
 import { CastsSort, FarcasterCast } from "@/lib/types";
 import { NextResponse } from "next/server";
+import { cache } from "react";
+
+export const revalidate = 60;
 
 export async function GET(
-  request: Request
+	request: Request,
 ): Promise<NextResponse<FarcasterCast[]>> {
-  const url = new URL(request.url);
-  const fid = url.searchParams.get("fid");
-  const fidParsed = fid ? parseInt(fid) : undefined;
-  const parentUrl = url.searchParams.get("parentUrl") || undefined;
-  const page = parseInt(url.searchParams.get("page") || "1");
-  const sort = url.searchParams.get("sort") as CastsSort;
-  const onlyParents = url.searchParams.get("all") !== "true";
-  const urlParsed = url.searchParams.get("url")
-    ? decodeURI(url.searchParams.get("url") as string)
-    : undefined;
-  const query = url.searchParams.get("query")
-    ? (url.searchParams.get("query") as string).length > 5
-      ? decodeURIComponent(url.searchParams.get("query") as string)
-      : ` ${decodeURIComponent(url.searchParams.get("query") as string)} `
-    : "";
+	const url = new URL(request.url);
+	const fid = url.searchParams.get("fid");
+	const fidParsed = fid ? parseInt(fid) : undefined;
+	const parentUrl = url.searchParams.get("parentUrl") || undefined;
+	const page = parseInt(url.searchParams.get("page") || "1");
+	const sort = url.searchParams.get("sort") as CastsSort;
+	const onlyParents = url.searchParams.get("all") !== "true";
+	const urlParsed = url.searchParams.get("url")
+		? decodeURI(url.searchParams.get("url") as string)
+		: undefined;
+	const query = url.searchParams.get("query")
+		? (url.searchParams.get("query") as string).length > 5
+			? decodeURIComponent(url.searchParams.get("query") as string)
+			: ` ${decodeURIComponent(url.searchParams.get("query") as string)} `
+		: "";
 
-  let response = [];
-  if (sort === CastsSort.Home) {
-    const viewerFid = url.searchParams.get("viewerFid")
-      ? parseInt(url.searchParams.get("viewerFid") as string)
-      : undefined;
-    response = await getCastsResponseByHotness(
-      page,
-      onlyParents,
-      parentUrl,
-      viewerFid
-    );
-  } else if (sort === CastsSort.Hot) {
-    response = await getCastsResponseByHotness(page, onlyParents, parentUrl);
-  } else if (sort === CastsSort.Top || sort === CastsSort.TopReplies) {
-    const time = url.searchParams.get("time") as
-      | "hour"
-      | "day"
-      | "week"
-      | "month"
-      | "year"
-      | "all";
-    response = await getCastsResponseByTopLikes(
-      page,
-      sort === CastsSort.TopReplies,
-      time || "all",
-      parentUrl,
-      fidParsed,
-      urlParsed,
-      query
-    );
-  } else if (sort === CastsSort.New || sort === CastsSort.NewReplies) {
-    response = await getCastsResponseByNewness(
-      page,
-      sort === CastsSort.NewReplies,
-      parentUrl,
-      fidParsed,
-      urlParsed,
-      query
-    );
-  }
+	let response = [];
+	if (sort === CastsSort.Home) {
+		const viewerFid = url.searchParams.get("viewerFid")
+			? parseInt(url.searchParams.get("viewerFid") as string)
+			: undefined;
+		response = await getCachedResponseByHotness(
+			page,
+			onlyParents,
+			parentUrl,
+			viewerFid,
+		);
+	} else if (sort === CastsSort.Hot) {
+		response = await getCachedResponseByHotness(page, onlyParents, parentUrl);
+	} else if (sort === CastsSort.Top || sort === CastsSort.TopReplies) {
+		const time = url.searchParams.get("time") as
+			| "hour"
+			| "day"
+			| "week"
+			| "month"
+			| "year"
+			| "all";
+		response = await getCachedResponseByTopLikes(
+			page,
+			sort === CastsSort.TopReplies,
+			time || "all",
+			parentUrl,
+			fidParsed,
+			urlParsed,
+			query,
+		);
+	} else if (sort === CastsSort.New || sort === CastsSort.NewReplies) {
+		response = await getCastsResponseByNewness(
+			page,
+			sort === CastsSort.NewReplies,
+			parentUrl,
+			fidParsed,
+			urlParsed,
+			query,
+		);
+	}
 
-  return NextResponse.json(response);
+	return NextResponse.json(response);
 }
+
+const getCachedResponseByHotness = cache(
+	async (
+		page: number,
+		onlyParents: boolean,
+		parentUrl?: string,
+		viewerFid?: number,
+	) => {
+		return await getCastsResponseByHotness(
+			page,
+			onlyParents,
+			parentUrl,
+			viewerFid,
+		);
+	},
+);
+
+const getCachedResponseByTopLikes = cache(
+	async (
+		page: number,
+		onlyParents: boolean,
+		time: "hour" | "day" | "week" | "month" | "year" | "all",
+		parentUrl?: string,
+		fid?: number,
+		url?: string,
+		query?: string,
+	) => {
+		return await getCastsResponseByTopLikes(
+			page,
+			onlyParents,
+			time,
+			parentUrl,
+			fid,
+			url,
+			query,
+		);
+	},
+);
